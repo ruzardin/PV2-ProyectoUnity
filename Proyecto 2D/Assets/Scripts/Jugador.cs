e665d096c8d5453ef00e2dfa8b39af5885825629
ElChuzo <lautibragado@gmail.com>
2025-09-07 03:32:29 -0300

    Configuracion de mecanicas base
    Se crean en los prefab enemigo y jugador los scripts necesarios para el correcto funcionamiento basico del juego
    Jugador adquiere la propiedad vida y un collider2d para detectar el contacto con el enemigo
    Enemigo adquiere la propiedad puntos, para restarle vida al Jugador

---
+++ Proyecto 2D/Assets/Scripts/Jugador.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    [Header("Configuracion")]
+    [SerializeField] private float vida = 5f;
+
+    public void ModificarVida(float puntos)
+    {
+        vida += puntos;
+        Debug.Log(EstasVivo());
+    }
+
+
+    private bool EstasVivo()
+    {
+        return vida > 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Meta")) { return; }
+
+        Debug.Log("GANASTE");
+    }
+}
